a4bfa5eabc2827963d11041f3329787c20ba6b34
jas408 <agenorjasleen@gmail.com>
2026-03-08 19:05:04 -0400

    updated file structure

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,6 +1,7 @@
 import {  Routes, Route, Link, BrowserRouter } from 'react-router-dom' // for routing in react
-import Login from "./Login";
-import Signup from "./Signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Home } from './pages/Home';
 
 function App() {
   return (
@@ -11,12 +12,13 @@ function App() {
           <Link to="/signup">Sign Up</Link>
         </nav>
         
-        <h1>Gym App</h1>
+        {/* <h1>Gym App</h1>x  */}
 
         {/* container for all routes */}
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
         </Routes>
 
       </div>
--- frontend/src/app/pages/Signup.jsx
+++ frontend/src/app/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { auth, db } from "./firebase";
+import { auth, db } from "../../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
 
