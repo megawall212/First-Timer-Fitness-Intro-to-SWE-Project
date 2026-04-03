389a32cef2066333154d13a5e7dfc00570fff539
dayap2404 <dayani.plasencia@ufl.edu>
2026-04-03 14:45:54 -0400

    Finished the login/signup UI with backend connection and built the header.

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,7 +1,11 @@
 import {  Routes, Route, Link, BrowserRouter } from 'react-router-dom' // for routing in react
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import SignIn from "./pages/Login";
+import SignUp from "./pages/Signup";
 import { Home } from './pages/Home';
+import Header from "./components/Header"; 
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   return (
@@ -11,13 +15,14 @@ function App() {
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </nav>
+        <Header />
         
         {/* <h1>Gym App</h1>x  */}
 
         {/* container for all routes */}
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<Home />} />
         </Routes>
 
